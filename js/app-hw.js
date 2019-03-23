0076e8a974aaadd21a2806bda6ffba2954234894
Andrew Verbitsky <averbitskiy5@gmail.com>
2019-03-23 18:11:23 +0200

    piano

---
+++ js/app-hw.js
@@ -0,0 +1,19 @@
+const pianoItem = document.querySelectorAll('.item');
+
+pianoItem.forEach(item => {
+	var pianoButtonA = document.getElementById('sound-A');
+	let pianoButtonS = document.getElementById('sound-S');
+	let pianoButtonD = document.getElementById('sound-D');
+	let pianoButtonF = document.getElementById('sound-F');
+	let pianoButtonG = document.getElementById('sound-G');
+	let pianoButtonH = document.getElementById('sound-H');
+	let pianoButtonJ = document.getElementById('sound-H');
+	let pianoButtonK = document.getElementById('sound-K');	
+	item.addEventListener('click', function playAudio() {
+		// pianoButtonA.play();
+		// pianoButtonS.play();		
+	})		
+});
+
+
+
