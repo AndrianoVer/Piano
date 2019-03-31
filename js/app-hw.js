7a1d1cf3fe8285b024983be2c853e7aa37059345
Andrew Verbitsky <averbitskiy5@gmail.com>
2019-03-31 22:31:52 +0300

    do on click mouse, on press button

--- js/app-hw.js
+++ js/app-hw.js
@@ -1,20 +1,48 @@
 
-const pianoItem = document.querySelectorAll('.item');
+// On mouse click;
+function soundA(){
+	document.getElementById('sound_A').play();
+}
 
-let pianoButtonA = document.getElementById('sound_A');
-let pianoButtonS = document.getElementById('sound_S');
-let pianoButtonD = document.getElementById('sound_D');
-let pianoButtonF = document.getElementById('sound_F');
-let pianoButtonG = document.getElementById('sound_G');
-let pianoButtonH = document.getElementById('sound_H');
-let pianoButtonJ = document.getElementById('sound_J');
-let pianoButtonK = document.getElementById('sound_K');
+function soundS() {
+	document.getElementById('sound_S').play();
+}
 
-pianoItem.forEach(item => {			
-	item.addEventListener('click', function playAudio() {
-		pianoButtonA.play();		
-	})	
-});
+function soundD() {
+	document.getElementById('sound_D').play();
+}
+
+function soundF(){
+	document.getElementById('sound_F').play();
+}
+
+function soundG() {
+	document.getElementById('sound_G').play();
+}
+
+function soundH() {
+	document.getElementById('sound_H').play();
+}
+
+function soundJ() {
+	document.getElementById('sound_J').play();
+}
+
+function soundK() {
+	document.getElementById('sound_K').play();
+}
+let pianoButtonA = document.getElementById('button-A').addEventListener('click', soundA)
+let pianoButtonS = document.getElementById('button-S').addEventListener('click', soundS)
+let pianoButtonD = document.getElementById('button-D').addEventListener('click', soundD)
+let pianoButtonF = document.getElementById('button-F').addEventListener('click', soundF)
+let pianoButtonG = document.getElementById('button-G').addEventListener('click', soundG)
+let pianoButtonH = document.getElementById('button-H').addEventListener('click', soundH)
+let pianoButtonJ = document.getElementById('button-J').addEventListener('click', soundJ)
+let pianoButtonK = document.getElementById('button-K').addEventListener('click', soundK)
+
+
+
+// Press on buttons (A-K);
 
 document.addEventListener('keydown', function(press){
 	switch (press.keyCode){
